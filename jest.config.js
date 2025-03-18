3c6e921b82bec5d1a0120216e29e6e8f8a42ebb4
Zachary Belford <belfordz66@gmail.com>
2025-03-18 12:55:10 -0700

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testTimeout: 5000000, // the more node involve in testing, the higher the timeout requires
   verbose: true,
   roots: ['<rootDir>/test/unit'],
   setupFiles: ['<rootDir>/test/unit/setup.ts'],
